ec2b3caaad8f95ca9c43be63d20545e945b79d93
jasdfon <jasdfon@protonmail.com>
2025-12-03 22:46:05 +0000

    cleanup

--- 2025/day03/gold.c
+++ 2025/day03/gold.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BATTERY_SIZE 12
+#define AOC_BATTERY_SIZE 12
 
 void updateBattery(char* bat, char c){
     int cAvail = 1;
-    for(int i = 0; i < BATTERY_SIZE - 1 && cAvail; i++){
+    for(int i = 0; i < AOC_BATTERY_SIZE - 1 && cAvail; i++){
         if(bat[i] < bat[i+1]){
-            while(i < BATTERY_SIZE - 1){
+            while(i < AOC_BATTERY_SIZE - 1){
                 bat[i] = bat[i+1];
                 i++;
             }
-            bat[BATTERY_SIZE - 1] = c;
+            bat[AOC_BATTERY_SIZE - 1] = c;
             cAvail = 0;
         }
     }
-    if(cAvail && c > bat[BATTERY_SIZE - 1]){
-        bat[BATTERY_SIZE - 1] = c;
+    if(cAvail && c > bat[AOC_BATTERY_SIZE - 1]){
+        bat[AOC_BATTERY_SIZE - 1] = c;
     }
 }
 
 void initBattery(char* bat, FILE *fp){
-    for(int i = 0; i < BATTERY_SIZE; i++){
+    for(int i = 0; i < AOC_BATTERY_SIZE; i++){
         bat[i] = fgetc(fp);
     }
 }
@@ -30,8 +30,8 @@ int main(int argc, char *argv[]){
     FILE *fp = fopen(argv[1], "r");
     if (!fp) return 1;
 
-    char bat[BATTERY_SIZE+1]; char c;
-    bat[BATTERY_SIZE] = '\0';
+    char bat[AOC_BATTERY_SIZE+1]; char c;
+    bat[AOC_BATTERY_SIZE] = '\0';
     long gold = 0;
     initBattery(bat, fp);
     while((c = fgetc(fp)) != EOF){
--- 2025/day03/silver.c
+++ 2025/day03/silver.c
@@ -1,30 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define AOC_CHAR_BUFFER_SIZE 128
-
 int main(int argc, char *argv[]){
     FILE *fp = fopen(argv[1], "r");
     if (!fp) return 1;
 
-    char battery[3]; char c;
-    battery[0] = fgetc(fp);
-    battery[1] = fgetc(fp);
-    battery[2] = '\0';
+    char bat[3]; char c;
+    bat[0] = fgetc(fp);
+    bat[1] = fgetc(fp);
+    bat[2] = '\0';
     long silver = 0;
     int changed = 1;
     while((c = fgetc(fp)) != EOF){
         if(c == '\n'){
-            silver += atoi(battery);
-            battery[0] = fgetc(fp);
-            battery[1] = fgetc(fp);
+            silver += atoi(bat);
+            bat[0] = fgetc(fp);
+            bat[1] = fgetc(fp);
         }
-        if(changed && battery[1] > battery[0]){
-            battery[0] = battery[1];
-            battery[1] = c;
+        if(changed && bat[1] > bat[0]){
+            bat[0] = bat[1];
+            bat[1] = c;
             changed = 1;
-        }else if(c > battery[1]){
-            battery[1] = c;
+        }else if(c > bat[1]){
+            bat[1] = c;
             changed = 1;
         }else{
             changed = 0;
